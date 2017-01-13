8d20bbe4200a780c245ff86696988c4bdb6e48ea
Nima <nima1367@gmail.com>
2017-01-13 09:11:00 -0800

    Meshes are added to a folder

--- VolumeCalculator/VolumeCalculator.py
+++ VolumeCalculator/VolumeCalculator.py
@@ -8,3 +8,14 @@ def CalculateVolume(faces):
         tetrahedronVolume = face.Area * (sum(face.Normal[:]*face.Vertices[0][:])) /3.0
         volume += tetrahedronVolume
     return volume
+
+def main():
+    mesh = np.load('../Meshes/unit_cube_qppp.npy')
+    faces = list()
+    for face in mesh:
+        faces.append(Face(face))
+    volume = CalculateVolume(faces)
+    input()
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
