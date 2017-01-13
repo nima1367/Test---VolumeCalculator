41955c619871c5f366c9641141a155324bb5f6e5
Nima <nima1367@gmail.com>
2017-01-13 09:44:52 -0800

    Comments are added

--- VolumeCalculator/Face.py
+++ VolumeCalculator/Face.py
@@ -2,7 +2,13 @@ import numpy as np
 import math
 
 class Face:
+
+    """ Constructor of the Face class"""
     def __init__(self, vertices):
+
+        """ NormalFinder function finds the normal vector
+        of the given polygonal face (the result is not normalizd)  
+        """
         def NormalFinder(vertices):
              edge1 = np.subtract(vertices[0], vertices[2])
              edge2 = np.subtract(vertices[1], vertices[0])
@@ -12,5 +18,8 @@ class Face:
         initialNormal = NormalFinder(vertices)
         crossMagn = math.sqrt(sum(initialNormal[i]*initialNormal[i] for i in range(3)))
         
+        # Normalize the obtained normal vector:
         self.Normal = np.divide(initialNormal, crossMagn)
-        self.Area = crossMagn/2.0
+        
+        # the area of a triangle is the magnitude of the crosss product of two of its edges
+        self.Area = crossMagn/2.0 
--- VolumeCalculator/VolumeCalculator.py
+++ VolumeCalculator/VolumeCalculator.py
@@ -1,19 +1,31 @@
 import sys
 import numpy as np 
+import unittest
 from Face import Face
 
+""" CalculateVolume function takes the faces of a tessellated model
+and calculates its volume
+"""
 def CalculateVolume(faces):
     volume = 0
     for face in faces:
+        # for every face, the volume of the tetrahedrun is calculated
+        # the tetrahedron is made with 4 vertices: 3 vertices of the face and the origin
         tetrahedronVolume = face.Area * (sum(face.Normal[:]*face.Vertices[0][:])) /3.0
+        
+        # depending on the direction of the normal vector, the volume of the tertrahedron  
+        # can be either negative or positive
         volume += tetrahedronVolume
     return volume
 
 def main():
+    # meshes dictionary contains the name of the meshes (keys) and the imported arrays(value)
     meshes = {'unit_cube_qppp': np.load('../Meshes/unit_cube_qppp.npy'),
               'shell': np.load('../Meshes/shell.npy'),
               'Robot_Maker_Faire_65pc': np.load('../Meshes/Robot_Maker_Faire_65pc.npy')}
     
+    # PreCalculatedValues dictionary contains the name of the meshes (keys) and the precalculated 
+    # values of their volume (value)
     preCalculatedValues = {'unit_cube_qppp': 1.0,
                           'shell': 3.6586764273,
                           'Robot_Maker_Faire_65pc': 43677.4258266209}
